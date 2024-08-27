913c2b3114c03b42120282473532cc3688fb5a2e
SSonikaa <sainusonika@gmail.com>
2024-08-27 15:53:29 +0530

    adding files

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -6,14 +6,13 @@ export const SocketContext= createContext()
 export const  useSocketContext=()=>{
     return useContext(SocketContext);
 }
-
 export const SocketContextProvider=({children})=>{
     const [socket,setSocket]=useState(null);
     const [onlineUsers,setOnlineUsers]= useState([]);
     const {authUser}= useAuthContext();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:8000",{
+            const socket = io(" https://twirlychat.onrender.com",{
                 query:{
                     userId: authUser._id,
                 }
